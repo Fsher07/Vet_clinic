207dbc5396e6ba9666dcff9a270393b86888f8d8
Fsher07 <furkansalimm@gmail.com>
2022-06-09 13:14:29 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,3 +1,5 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals ( id INT PRIMARY KEY NOT NULL, name VARCHAR, date_of_birth DATE, escape_attempts INT, neutered BOOLEAN, weight_kg DECIMAL);
+
+ALTER TABLE animals ADD COLUMN species VARCHAR(75);
