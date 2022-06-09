3c8c951f1bb9e518f76e59b9f94a3a6edcd0171f
Fsher07 <furkansalimm@gmail.com>
2022-06-09 17:44:50 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -10,3 +10,27 @@ ALTER TABLE animals RENAME COLUMN species TO unspecified;
 SELECT unspecified FROM animals;
 ROLLBACK;
 SELECT species FROM animals;
+
+CREATE TABLE owners ( id SERIAL, full_name VARCHAR(75), age INT, PRIMARY KEY(id));
+CREATE TABLE species  ( id SERIAL, name VARCHAR(75), PRIMARY KEY(id));
+
+/*Create table named owners*/
+CREATE TABLE owners ( id SERIAL, full_name VARCHAR(75), age INT, PRIMARY KEY(id));
+
+/*Create table named species*/
+CREATE TABLE species  ( id SERIAL, name VARCHAR(75), PRIMARY KEY(id));
+
+/*In animals table, id is set autoincremented primary key*/
+BEGIN;
+ALTER TABLE animals DROP COLUMN id;
+ALTER TABLE animals ADD COLUMN id SERIAL PRIMARY KEY;
+COMMIT;
+
+/*Remove species column*/
+ALTER TABLE animals DROP COLUMN species;
+
+/*Column references species table*/
+ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species (id);
+
+/*Column references owners table*/
+ALTER TABLE animals ADD COLUMN owner_id INT REFERENCES owners (id);
