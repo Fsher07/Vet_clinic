3abf99b45115eb2e89c8c02d9ec15163ef9cc965
Fsher07 <furkansalimm@gmail.com>
2022-06-09 13:35:52 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -3,3 +3,10 @@
 CREATE TABLE animals ( id INT PRIMARY KEY NOT NULL, name VARCHAR, date_of_birth DATE, escape_attempts INT, neutered BOOLEAN, weight_kg DECIMAL);
 
 ALTER TABLE animals ADD COLUMN species VARCHAR(75);
+
+/*Begin a transaction and rollback to remove changes*/
+BEGIN;
+ALTER TABLE animals RENAME COLUMN species TO unspecified;
+SELECT unspecified FROM animals;
+ROLLBACK;
+SELECT species FROM animals;
